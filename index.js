26e58f3f1bdb7e6807feef4ab01ef60ef99e85a4
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-04 06:47:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var high=4, score=0;
+var readlineSync = require("readline-sync");
+var name = readlineSync.question("Who has awakened the almighty me? ");
+var choice = readlineSync.keyInYN("Welcome "+name+"! Would you care for a little quiz?");
+if(!choice)
+  console.log("Understandable. Have a great day.");
+else
+{
+  ques=["What is the name of my creator?",
+        "which season is his favorite? ",
+        "What kind of weather can my creator not stand?",
+        "Does my creator like chocolates?",
+        "Does he like milk chocolates over Dark chocolates?",
+        "Does he like cool things or cute things?",
+        "What is the powerhouse of the cell?",
+        "Thicc ___ saves lives."
+        ]
+  ans = ["Sameer",
+        "Rainy",
+        "Sunny",
+        "Yes",
+        "No",
+        "Cute",
+        "Mitochondria",
+        "Thighs"
+       ]
+       for(var i=0;i<ques.length;i++)
+       {
+         var userAns = readlineSync.question(ques[i]);
+         if(userAns.toUpperCase()===ans[i].toUpperCase())
+        {
+          score+=1;
+          console.log("DING DING DING! Correct answer!!");
+        }
+        else
+          console.log("OOPS! Wrong answer");
+        var con=readlineSync.keyInYN("Would you like to continue?");
+        if(!con)
+          break;
+       }
+       console.log("Your score is: "+score);
+       if(score>=high)
+        {  
+          console.log("It is a new high score!!!");
+          high=score;
+        }
+        else
+        {
+          console.log("Oooh!you missed the high score by "+(high-score)+" points!");
+        }
+}
